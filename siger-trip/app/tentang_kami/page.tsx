3ed60c117b313693e621191422868cb717861106
Dzakyyy09 <muhammad_dzaky_akmal@teknokrat.ac.id>
2026-06-12 06:30:56 +0700

    akmal: mengganti icon pada bagian Fokus Layanan Kami

--- siger-trip/app/tentang_kami/page.tsx
+++ siger-trip/app/tentang_kami/page.tsx
@@ -91,7 +91,7 @@ export default function AboutPage() {
           </div>
 
           <div>
-            <div className="text-4xl mb-6 text-slate-400">✱</div>
+            <div className="text-4xl mb-6 text-slate-400">✦</div>
             <h3 className="card-title text-2xl mb-6">Fokus Layanan Kami</h3>
             <div className="space-y-5">
               <div>
