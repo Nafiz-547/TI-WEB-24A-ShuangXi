3400b104694e84bb79ff5f853baf1936e4dc08a1
rizkypratama23133 <akasarizky590@gmail.com>
2026-06-11 23:28:28 +0700

    tama: menambah card terbaru di sorotan kuliner yaitu Pindang Patin

--- siger-trip/components/SorotanKuliner.tsx
+++ siger-trip/components/SorotanKuliner.tsx
@@ -26,6 +26,14 @@ export default function SorotanKuliner() {
       img: '/images/Bakso Son Haji Sony.webp',
       alt: 'Bakso Son Haji Sony Lampung',
       href: 'https://www.google.com/search?q=Bakso+Son+Haji+Sony'
+    },
+    {
+      id: 'pindang-patin',
+      title: 'Pindang Patin',
+      desc: 'Pindang Patin adalah masakan tradisional Lampung yang terbuat dari ikan patin segar yang dipindang dengan kuah rempah hangat berisi lengkuas, jahe, dan cabai. Hidangan ini menjadi favorit karena cita rasa kuah yang gurih dan tradisional, serta berasal dari daerah-daerah seperti Lampung Selatan dan Lampung Tengah yang kaya dengan perairan ikan patin.',
+      img: '/images/Pindang Patin.webp',
+      alt: 'Pindang Patin Lampung',
+      href: 'https://www.google.com/search?q=Pindang+Patin+Lampung'
     }
   ];
 
